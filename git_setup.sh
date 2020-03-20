99c6488cc0e1031a660c7976d12733f36255ff0d
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-20 21:50:28 +0100

    update git_setup to accept -u -e -k parmaters username(full name) email gpgkeyid

--- git_setup.sh
+++ git_setup.sh
@@ -1,13 +1,40 @@
 IFS=$'\n'
-echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
-read full_name
 
-echo "Type in your email address (the one used for your GitHub account): "
-read email
+while getopts "u:e:k:" opt; do
+  case $opt in
+    u) full_name="$OPTARG"
+    ;;
+    e) email="$OPTARG"
+    ;;
+    k) keyid="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done  
 
-echo "To list GPG KEYS  do ::   gpg --list-secret-keys --keyid-format LONG "
-echo "Type in your GPG KEY ID (the one used for your GitHub account): "
-read keyid
+if [ -z "$full_name" ]
+then
+  echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
+  read full_name
+fi
+
+if  [ -z "$email" ]
+then
+  echo "Type in your email address (the one used for your GitHub account): "
+  read email
+fi
+
+if [ -z "$keyid" ]
+then
+  echo "To list GPG KEYS  do ::   gpg --list-secret-keys --keyid-format LONG "
+  echo "Type in your GPG KEY ID (the one used for your GitHub account): "
+  read keyid
+fi
+
+echo "Github Username   : ${full_name}" 
+echo "Github Email      : ${email}"
+echo "Github GPG key ID : ${keyid}"
 
 git config --global user.email $email
 git config --global user.name $full_name

246106c7b3beeef5adea0872a4ac80a522fdd8ca
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-24 11:16:50 +0200

    update get-install-files.sh

--- bin/scripts/arch/get-install-files.sh
+++ bin/scripts/arch/get-install-files.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/arch-install.sh
-curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/inside-chroot.sh
-curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/after-reboot.sh
+curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/arch-install.sh > arch-install.sh
+curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/inside-chroot.sh > inside-chroot.sh
+curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/after-reboot.sh > /after-reboot.sh
 
 chmod +x arch-install.sh
 chmod +x inside-chroot.sh
 chmod +x after-reboot.sh
 
-curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/arch-install.config
-curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/packages.config
+curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/arch-install.config > arch-install.config
+curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/packages.config > packages.config

45700d99e2384d093df2b2af6d73e07fe3fd5380
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-24 11:26:23 +0200

    update get-install-files.sh

--- bin/scripts/arch/get-install-files.sh
+++ bin/scripts/arch/get-install-files.sh
@@ -2,7 +2,7 @@
 
 curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/arch-install.sh > arch-install.sh
 curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/inside-chroot.sh > inside-chroot.sh
-curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/after-reboot.sh > /after-reboot.sh
+curl https://raw.githubusercontent.com/abstracts33d/dotfiles/master/bin/scripts/arch/after-reboot.sh > after-reboot.sh
 
 chmod +x arch-install.sh
 chmod +x inside-chroot.sh
